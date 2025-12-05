3ccb42fac0a585486365db78c89b7c4932b3034c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-12-05 16:07:55 +0100

    refactor: improve javelite implementation
    work on #287

--- spring-ai/spring-ai-agent/src/test/java/JtAgentExecutorApp.java
+++ spring-ai/spring-ai-agent/src/test/java/JtAgentExecutorApp.java
@@ -151,6 +151,28 @@ public class JtAgentExecutorApp {
     }
 
 
+    record Model( Provider provider, String name ) {
+        enum Provider {
+            OLLAMA,
+            OPENAI,
+            GITHUB,
+            VERTEX
+        }
+
+        public String apiKeyPlaceholder() {
+            return switch( provider ) {
+                case OPENAI -> "OpenAI Api Key";
+                case OLLAMA -> "";
+                case VERTEX -> "Google Cloud Project Id";
+                case GITHUB -> "Github Model Token";
+            };
+        }
+
+        @Override
+        public String toString() {
+            return "%s:%s".formatted( provider.name().toLowerCase(), name );
+        }
+    }
 
     Optional<ChatModel> getChatModelView() {
 
@@ -163,28 +185,24 @@ public class JtAgentExecutorApp {
             if (cloud) {
                 var cloudModelCols = Jt.columns(2).key("cloud-model-cols").use();
                 var model = Jt.radio("Available models",
-                        List.of("gpt-4o-mini",
-                                "gh_models:gpt-4o-mini",
-                                "vertex:gemini-2.5-pro"))
+                        List.of(
+                                new Model(Model.Provider.OPENAI, "gpt-4o-mini"),
+                                new Model(Model.Provider.GITHUB,"gpt-4o-mini"),
+                                new Model(Model.Provider.VERTEX,"gemini-2.5-pro")
+                        ))
                         .use(cloudModelCols.col(0));
-
-                Supplier<String> modelPlaceholder = () -> "gpt-4o-mini".equals(model) ?
-                        "OpenAI Api Key":
-                        "gh_models:gpt-4o-mini".equals(model) ?
-                                "Github Model Token" :
-                                "Google Cloud Project Id";
 
                 var apikey = Jt.textInput("API KEY:")
                         .type("password")
                         .labelVisibility(JtComponent.LabelVisibility.HIDDEN)
-                        .placeholder(  modelPlaceholder.get() )
+                        .placeholder( model.apiKeyPlaceholder() )
                         .width(500)
                         .use(cloudModelCols.col(1));
                 if (apikey.isEmpty()) {
-                    Jt.error("%s cannot be null".formatted( modelPlaceholder.get() )).use();
+                    Jt.error("%s cannot be null".formatted( model.apiKeyPlaceholder() )).use();
                     return Optional.empty();
                 }
-                if( model.startsWith("vertex:") ) {
+                if( model.provider()  == Model.Provider.VERTEX  ) {
                     var gcpLocation = Jt.textInput("Google Cloud Location:")
                             .labelVisibility(JtComponent.LabelVisibility.HIDDEN)
                             .placeholder( "Google Cloud Location" )
@@ -193,22 +211,23 @@ public class JtAgentExecutorApp {
                         Jt.error("Google Cloud Location cannot be null").use();
                         return Optional.empty();
                     }
-                    return Optional.of(AiModel.GEMINI.chatModel(model.substring(7),
+                    return Optional.of(AiModel.GEMINI.chatModel(model.name(),
                             Map.of("GOOGLE_CLOUD_PROJECT", apikey,
                                     "GOOGLE_CLOUD_LOCATION", gcpLocation)));
 
                 }
-                if( model.startsWith("gh_models:") ) {
-                    return Optional.of(AiModel.GITHUB_MODEL.chatModel(model.substring(10), Map.of("GITHUB_MODELS_TOKEN", apikey)));
+                if( model.provider() == Model.Provider.GITHUB  ) {
+                    return Optional.of(AiModel.GITHUB_MODEL.chatModel(model.name(), Map.of("GITHUB_MODELS_TOKEN", apikey)));
                 }
-                return Optional.of(AiModel.OPENAI.chatModel(model, Map.of("OPENAI_API_KEY", apikey)));
+                return Optional.of(AiModel.OPENAI.chatModel(model.name(), Map.of("OPENAI_API_KEY", apikey)));
             } else {
                 var model = Jt.radio("Available models", List.of(
-                                "qwen2.5:7b",
-                                "qwen3:8b",
-                                "gpt-oss:20b"))
+                                new Model(Model.Provider.OLLAMA,"qwen2.5:7b"),
+                                new Model(Model.Provider.OLLAMA,"qwen3:8b"),
+                                new Model(Model.Provider.OLLAMA,"gpt-oss:20b")
+                        ))
                         .use();
-                return Optional.of(AiModel.OLLAMA.chatModel(model));
+                return Optional.of(AiModel.OLLAMA.chatModel(model.name()));
             }
         }
         catch( Throwable ex ) {
