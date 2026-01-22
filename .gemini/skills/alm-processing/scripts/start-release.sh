90e1f704e6ee4746e281f5f1ae6af19ac5c85c6e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-01-22 18:10:42 +0100

    chore(SKILL): fix script command

--- .gemini/skills/alm-processing/scripts/start-release.sh
+++ .gemini/skills/alm-processing/scripts/start-release.sh
@@ -2,7 +2,7 @@
 if [ $# -gt 0 ]; then
     ver="$1"
     echo "version: $ver"
-    git flow release finish "$ver"
+    git flow release start "$ver"
 else
     echo "no version provided abort overall processing"
 fi
